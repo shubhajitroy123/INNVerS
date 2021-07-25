941a40242c49c022b4966ef648f9cf51794ae9f0
IACS CSU 2020 <87594404+iacs-csu-2020@users.noreply.github.com>
2021-07-25 13:44:03 +0000

    Update install_tool.sh

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -11,9 +11,9 @@ echo "Installing INNVerS"
 #installing tool
 apt update
 apt install -y python3 python3-pip #Python 3.7 or higher
+apt install libprotoc-dev protobuf-compiler #prerequisite for ONNX
 #apt install -y psmisc && #for killall, used in prepare_instance.sh
 pip3 install numpy #Numpy
 pip3 install ortools #Google OR-Tools for MILP optimization
-pip3 install libprotoc-dev protobuf-compiler #prerequisite for ONNX
 pip3 install onnx==1.8.1 #ONNX for .onnx file processing
 pip3 install z3-solver #z3 Theorem Prover for .vnnlib file processing
