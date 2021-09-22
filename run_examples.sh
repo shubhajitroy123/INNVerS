7fcc3e5ac64ecdfc4c6e8a823121cc31ff1bf881
Shubhajit Roy <81477286+shubhajitroy123@users.noreply.github.com>
2021-09-22 21:28:00 +0530

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -16,66 +16,56 @@ sudo chmod u+x run_instance.sh
 mkdir ../results
 
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_1.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_1.vnnlib ../results/result_acasxu_1.txt 15
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_1.vnnlib ../results/result_acasxu_1_1.txt 60
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_2.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_2.vnnlib ../results/result_acasxu_2.txt 15
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_2.vnnlib ../results/result_acasxu_1_2.txt 60
 ./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_3.vnnlib
-./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_3.vnnlib ../results/result_acasxu_3.txt 15
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_1_batch_2000.onnx ../examples/acasxu/prop_3.vnnlib ../results/result_acasxu_1_3.txt 60
 
-./prepare_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_1.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_1.vnnlib ../results/result_test_1.txt 15
-./prepare_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_2.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_2.vnnlib ../results/result_test_2.txt 15
-./prepare_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_3.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_3.vnnlib ../results/result_test_3.txt 15
-./prepare_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_4.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test.onnx ../examples/others/test_4.vnnlib ../results/result_test_4.txt 15
+./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_2_batch_2000.onnx ../examples/acasxu/prop_1.vnnlib
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_2_batch_2000.onnx ../examples/acasxu/prop_1.vnnlib ../results/result_acasxu_2_1.txt 60
+./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_2_batch_2000.onnx ../examples/acasxu/prop_2.vnnlib
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_2_batch_2000.onnx ../examples/acasxu/prop_2.vnnlib ../results/result_acasxu_2_2.txt 60
+./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_2_batch_2000.onnx ../examples/acasxu/prop_3.vnnlib
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_2_batch_2000.onnx ../examples/acasxu/prop_3.vnnlib ../results/result_acasxu_2_3.txt 60
 
-./prepare_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_1.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_1.vnnlib ../results/result_test_fixed_1.txt 15
-./prepare_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_2.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_2.vnnlib ../results/result_test_fixed_2.txt 15
-./prepare_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_3.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_3.vnnlib ../results/result_test_fixed_3.txt 15
-./prepare_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_4.vnnlib
-./run_instance.sh v1 "test" ../examples/others/test_fixed.onnx ../examples/others/test_4.vnnlib ../results/result_test_fixed_4.txt 15
+./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_3_batch_2000.onnx ../examples/acasxu/prop_1.vnnlib
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_3_batch_2000.onnx ../examples/acasxu/prop_1.vnnlib ../results/result_acasxu_3_1.txt 60
+./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_3_batch_2000.onnx ../examples/acasxu/prop_2.vnnlib
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_3_batch_2000.onnx ../examples/acasxu/prop_2.vnnlib ../results/result_acasxu_3_2.txt 60
+./prepare_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_3_batch_2000.onnx ../examples/acasxu/prop_3.vnnlib
+./run_instance.sh v1 acasxu ../examples/acasxu/ACASXU_run2a_1_3_batch_2000.onnx ../examples/acasxu/prop_3.vnnlib ../results/result_acasxu_3_3.txt 60
 
 clear
 
-echo -n "prop_1 of acasxu: "
-cat ../results/result_acasxu_1.txt
+echo -n "prop_1 of acasxu_1: "
+cat ../results/result_acasxu_1_1.txt
 echo " "
-echo -n "prop_2 of acasxu: "
-cat ../results/result_acasxu_2.txt
+echo -n "prop_2 of acasxu_1: "
+cat ../results/result_acasxu_1_2.txt
 echo " "
-echo -n "prop_3 of acasxu: "
-cat ../results/result_acasxu_3.txt
-echo " "
-
-echo -n "prop_1 of test: "
-cat ../results/result_test_1.txt
-echo " "
-echo -n "prop_2 of test: "
-cat ../results/result_test_2.txt
-echo " "
-echo -n "prop_3 of test: "
-cat ../results/result_test_3.txt
-echo " "
-echo -n "prop_4 of test: "
-cat ../results/result_test_4.txt
+echo -n "prop_3 of acasxu_1: "
+cat ../results/result_acasxu_1_3.txt
 echo " "
 
-echo -n "prop_1 of test_fixed: "
-cat ../results/result_test_fixed_1.txt
+echo -n "prop_1 of acasxu_2: "
+cat ../results/result_acasxu_2_1.txt
 echo " "
-echo -n "prop_2 of test_fixed: "
-cat ../results/result_test_fixed_2.txt
+echo -n "prop_2 of acasxu_2: "
+cat ../results/result_acasxu_2_2.txt
 echo " "
-echo -n "prop_3 of test_fixed: "
-cat ../results/result_test_fixed_3.txt
+echo -n "prop_3 of acasxu_2: "
+cat ../results/result_acasxu_2_3.txt
 echo " "
-echo -n "prop_4 of test_fixed: "
-cat ../results/result_test_fixed_4.txt
+
+echo -n "prop_1 of acasxu_3: "
+cat ../results/result_acasxu_3_1.txt
+echo " "
+echo -n "prop_2 of acasxu_3: "
+cat ../results/result_acasxu_3_2.txt
+echo " "
+echo -n "prop_3 of acasxu_3: "
+cat ../results/result_acasxu_3_3.txt
 echo " "
 
 rm -rf ../results
